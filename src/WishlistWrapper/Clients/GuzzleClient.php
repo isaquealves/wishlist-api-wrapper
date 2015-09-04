bd290fe5858e28b4e2c126543d771b495001b02d
Isaque Alves <isaquealves@gmail.com>
2015-09-04 13:12:30 -0300

    Improves GuzzleClient

--- src/WishlistWrapper/Clients/GuzzleClient.php
+++ src/WishlistWrapper/Clients/GuzzleClient.php
@@ -2,75 +2,73 @@
 
 namespace WishlistWrapper\Clients;
 
-use Guzzlehttp\Client;
+use GuzzleHttp\Client;
 
 class GuzzleClient
 {
-	protected $client;
+    protected $client;
 
-	public function __construct()
-	{
+    public function __construct($baseUrl)
+    {
 
-	}
+        $this->client =  new Client(
+            [
+                'base_url' => $baseUrl
+            ]
+        );
 
-	public function init($baseUrl)
-	{
+        return $this;
 
-		$this->client =  new Client(
-			[
-				'base_url' => $this->baseUrl
-			]
-		);
+    }
 
-		return $this->client;
-	}
+    /**
+     * Build and executes a post request over wishlist api address
+     * @param  [type] $fragmentUrl            The fragment url which have to be accessed
+     * @param  [type] $requestBody            Request body content
+     * @param  [type] $returnType             Type of return
+     * @return [type]                         [description]
+     */
+    public function post($fragmentUrl, $requestBody, $returnType)
+    {
+        $request = $this->client->createRequest(
+            'POST',
+            $fragmentUrl,
+            [
+                'body' => $requestBody
+            ]
+        );
+       
+        $response = $this->client->send($request);
 
-	public function post($fragmentUrl, $requestQueryParameters, $requestBody, $returnType)
-	{
-		$request = $this->client->createRequest(
-			'POST',
-			$fragmentUrl,
-			'query' => $requestQueryParameters,
-			'body' => $requestBody
-		);
-
-
-		$response = $this->client->send($request);
-
-		switch($returnType) {
-
-			case 'xml':
-				return $response->xml();
-				break;
-			case 'php':
-				return serialize($response->json());
-				break;
-			default:
-				return $response->json();
-		}
-	}
+        return $response;
+    }
 
 
-	public function get($fragmentUrl, $requestQueryParameters, $returnType)
-	{
-		$request = $this->client->createRequest(
-			'GET',
-			$fragmentUrl,
-			'query' => $queryParameters
-		);
+    public function get($fragmentUrl, $requestQueryParameters, $returnType)
+    {
+        $request = $this->client->createRequest(
+            'GET',
+            $fragmentUrl,
+            ['query' => $queryParameters]
+        );
 
-		$response = $this->client->send($request);
+        $response = $this->client->send($request);
 
-		switch($returnType) {
+        return $this->returns($response,$returnType);
+    }
 
-			case 'xml':
-				return $response->xml();
-				break;
-			case 'php':
-				return serialize($response->json());
-				break;
-			default:
-				return $response->json();
-		}
-	}
+    private function returns($response, $returnType)
+    {
+        switch($returnType) {
+
+            case 'xml':
+                return $response->xml();
+                break;
+            case 'php':
+                return serialize($response->json());
+                break;
+            default:
+                return $response->json();
+        }
+    }
 }
